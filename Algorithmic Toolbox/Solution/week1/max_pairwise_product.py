79aad41f5ea1d6e45f2850a0653de46f0eb9c900
Ali Setareh Kokab <ali.setarehkokab97@gmail.com>
2021-10-06 18:26:02 +0330

    max_pairwise_product

---
+++ Algorithmic Toolbox/Solution/week1/max_pairwise_product.py
@@ -0,0 +1,11 @@
+def max_pairwise_product(numbers):
+    numbers_array = np.array(numbers)
+    arg_max1 = np.argmax(numbers)
+    max1=numbers[arg_max1]
+    numbers_array = np.delete(numbers_array,arg_max1)
+    max2 = max(numbers_array)
+    return(max1*max2)
+
+input_n = int(input())
+input_numbers = [int(x) for x in input().split()]
+print(max_pairwise_product(input_numbers))
