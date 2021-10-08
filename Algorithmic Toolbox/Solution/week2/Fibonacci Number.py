4d4bf130e1bd02d4737c1ae013bea4c14a62ae9b
Ali Setareh Kokab <ali.setarehkokab97@gmail.com>
2021-10-08 18:42:31 +0330

    last digit of sum correct

--- Algorithmic Toolbox/Solution/week2/Fibonacci Number.py
+++ Algorithmic Toolbox/Solution/week2/Fibonacci Number.py
@@ -1,10 +1,13 @@
 # Fibonacci Number
 n = int(input())
-f = [0]*n
-f[0] = 0 
-f[1] = 1 
-for i in range(2,n):
-    f[i] = f[i-1]+f[i-2]
+if n<=1:
+    print(n)
+else:
+    f = [0]*n
+    f[0] = 0 
+    f[1] = 1 
+    for i in range(2,n):
+        f[i] = f[i-1]+f[i-2]
 
-print(f[n-1]+f[n-2])
+    print(f[n-1]+f[n-2])
 
--- Algorithmic Toolbox/Solution/week2/Last Digit of the Sum of Fibonacci Num.py
+++ Algorithmic Toolbox/Solution/week2/Last Digit of the Sum of Fibonacci Num.py
@@ -1,17 +1,28 @@
 # Last Digit of the Sum of Fibonacci Numbers
-n = int(input())
+inputs = int(input())
+n = inputs+2
 m = 10
 
 previous = 0
 current = 1
-
-for i in range(2,n+2):
+fib = []
+fib.append(previous)
+fib.append(current)
+flag=0
+for i in range(2,n):
     temp = current 
     current = (previous+current)%m
     previous = temp
-    
-if previous+current-1>=0:
-    print((previous+current-1)%m)
-
-else:
-    print(9)
+    fib.append(current) 
+    if current == 1 and previous == 0:
+     
+        period= i-1
+        if fib[int(n%period)]>0:
+            print(fib[int(n%period)]-1)
+           
+        else:
+            print(9)
+        flag=1
+        break
+if flag==0:
+    print((fib[n-1]+fib[n-2]-1)%m)
